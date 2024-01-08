6f69bc796423a97592cacb7413195718438b89ad
Wiktoria Stasiak <jan.kowalski@example.com>
2024-01-08 01:59:34 +0100

    Adding project

---
+++ project_8/the_snake_game/game.py
@@ -0,0 +1,51 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("dark blue")
+screen.tracer(0)
+screen.title("the Snake Game")
+
+
+snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        print("Yummy, yummy, yummy!")
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            gamie_is_on = False
+            scoreboard.game_over()
+
+
+
+
+screen.exitonclick()
+
---
+++ project_8/the_snake_game/scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align = 'center', font = ('Arial', 24, 'normal'))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game over! Score: {self.score}",  align = 'center', font = ('Arial', 24, 'normal'))
+
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
